932730a34600191593eb7f209f0dfcf49f3fb654
Tam Bui <tam@bracster.com>
2023-04-04 12:58:15 +0700

    test gc collector

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,15 +3,22 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { setFlagsFromString } from 'v8';
 import { runInNewContext } from 'vm';
 import { map } from 'lodash';
+import fetch from 'node-fetch'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/photos', { method: "Get" });
+  console.log('response',(response.json() as any).length)
+  console.log(
+    'Memory used before:',
+    map(process.memoryUsage(), (value, key) => `${key}: ${Math.round((value / 1024 / 1024) * 100) / 100} MB`)
+  );
     try {
     setFlagsFromString('--expose_gc');
     const gc = runInNewContext('gc'); // nocommit
@@ -21,7 +28,7 @@ export default function handler(
     process.exit();
   }
   console.log(
-    'Memory used: ',
+    'Memory used after:',
     map(process.memoryUsage(), (value, key) => `${key}: ${Math.round((value / 1024 / 1024) * 100) / 100} MB`)
   );
   return res.status(200).json({ name: 'John Doe' })
