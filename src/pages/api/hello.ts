845c540fe966c914f41db7c73fe2ae3b6547e08c
Tam Bui <tam@bracster.com>
2023-04-04 12:47:49 +0700

    test gc collector

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setFlagsFromString } from 'v8';
+import { runInNewContext } from 'vm';
+import { map } from 'lodash';
 
 type Data = {
   name: string
@@ -9,5 +12,17 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    try {
+    setFlagsFromString('--expose_gc');
+    const gc = runInNewContext('gc'); // nocommit
+    gc();
+  } catch (e) {
+    console.log('`node --expose-gc index.js`');
+    process.exit();
+  }
+  console.log(
+    'Memory used: ',
+    map(process.memoryUsage(), (value, key) => `${key}: ${Math.round((value / 1024 / 1024) * 100) / 100} MB`)
+  );
+  return res.status(200).json({ name: 'John Doe' })
 }
